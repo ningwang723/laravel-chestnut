071e500e3d02a777b5121de99242230e5d2b775c
ningwang723 <a33543015@163.com>
2021-08-02 10:57:07 +0800

    Update LaravelServiceProvider.php
    fixed path to configs, migrations and routes

--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -18,25 +18,17 @@ class LaravelServiceProvider extends AbstractServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/config/chestnut.php' => config_path('chestnut.php'),
+            __DIR__ . '/../config/chestnut.php' => config_path('chestnut.php'),
         ], 'config');
 
         $this->publishes([
-            __DIR__ . '/config/auth.php' => config_path('auth.php'),
+            __DIR__ . '/../config/auth.php' => config_path('auth.php'),
         ], 'config');
 
-        $this->publishes([
-            __DIR__ . '/assets' => public_path('/'),
-        ], 'public');
-
-        $this->publishes([
-            __DIR__ . '/translates' => resource_path('lang/vendor/chestnut'),
-        ], 'translate');
-
-        $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
         // $this->loadViewsFrom(__DIR__ . '/views/', 'chestnut');
         // $this->loadTranslationsFrom(__DIR__ . '/translates', 'chestnut');
-        $this->loadMigrationsFrom(__DIR__ . "/migrations");
+        $this->loadMigrationsFrom(__DIR__ . "/../migrations");
 
         if (!app()->runningInConsole()) {
             app("shell")->registerRepositories($this->app->config->get('chestnut.dashboard.nutsIn'), "app");
